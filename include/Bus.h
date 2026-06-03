8cbdd7868adf4ebdbc74fddffce6bd77064e9f6f
nsameron2 <91213588+nsameron2@users.noreply.github.com>
2026-06-03 01:01:26 -0700

    feat(bus): add basic safety requirements for memory, and declare scopes

--- include/Bus.h
+++ include/Bus.h
@@ -1,20 +1,33 @@
+#pragma once
+
 #include <cstdint>
 #include <array>
 
 
 // Our bus will connect the CPU, RAM, PPU, and loaded ROMs
 class Bus {
-    // Memory array, for the 2KB of RAM
-    std::array<uint8_t, 2048> memory;
-    
+public:
     // Function for writing into memory
     void write(uint16_t address, uint8_t data) {
-        // Slot our data into our given address
-        memory[address] = data;
+        // Make sure we're given valid parameters and insert our data into our given address
+        if(address < memory.size()) {
+            memory[address] = data;
+        } 
+        
     }
 
     // Function for reading memory
     uint8_t read(uint16_t address) const {
-        return memory[address];
+        // Make sure we're given valid parameters and read our data from the given address
+        // If the address is invalid, return 0x00
+        if(address < memory.size()) {
+            return memory[address];
+        }
+
+        return 0x00;
     }
+
+private:
+    // Memory array, for the 2KB of RAM
+    std::array<uint8_t, 2048> memory;
 };
