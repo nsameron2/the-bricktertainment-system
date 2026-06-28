68e5f0bee065de154c1977b5d71df7bb6c0368c4
nsameron2 <91213588+nsameron2@users.noreply.github.com>
2026-06-27 23:15:00 -0700

    feat(bus): implement memory mirroring and correct initialization of memory

--- include/Bus.h
+++ include/Bus.h
@@ -4,30 +4,32 @@
 #include <array>
 
 
+// For RAM mirroring
+constexpr uint16_t INTERNAL_RAM_SIZE = 0x0800;
+constexpr uint16_t INTERNAL_RAM_MASK = 0x07FF;
+constexpr uint16_t INTERNAL_RAM_MIRROR_END = 0x1FFF;
+
 // Our bus will connect the CPU, RAM, PPU, and loaded ROMs
 class Bus {
 public:
     // Function for writing into memory
     void write(uint16_t address, uint8_t data) {
-        // Make sure we're given valid parameters and insert our data into our given address
-        if(address < memory.size()) {
-            memory[address] = data;
+        if(address <= INTERNAL_RAM_MIRROR_END) {
+            memory[address & INTERNAL_RAM_MASK] = data;
         } 
         
     }
 
     // Function for reading memory
     uint8_t read(uint16_t address) const {
-        // Make sure we're given valid parameters and read our data from the given address
-        // If the address is invalid, return 0x00
-        if(address < memory.size()) {
-            return memory[address];
+        if(address <= INTERNAL_RAM_MIRROR_END) {
+            return memory[address & INTERNAL_RAM_MASK];
         }
 
         return 0x00;
     }
 
 private:
-    // Memory array, for the 2KB of RAM
-    std::array<uint8_t, 2048> memory;
+    // Memory array, for the 2KB of RAM. Initialize it to empty.
+    std::array<uint8_t, 2048> memory{};
 };
