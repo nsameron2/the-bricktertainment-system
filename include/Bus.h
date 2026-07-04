d8bf1109e670f33c069f5ca12b9a6530755f6449
nsameron2 <91213588+nsameron2@users.noreply.github.com>
2026-07-04 01:18:05 -0700

    refactor: move bus read and write to the cpp file, to preapre for cartrdige read/write implementation

--- include/Bus.h
+++ include/Bus.h
@@ -14,21 +14,9 @@ class Cartridge;
 // Our bus will connect the CPU, RAM, PPU, and loaded ROMs
 class Bus {
 public:
-    // Functions for writing or reading into memory. Delegate memory or cartridge writing/reading through the given address
-    void write(uint16_t address, uint8_t data) {
-        if(address <= INTERNAL_RAM_MIRROR_END) {
-            memory[address & INTERNAL_RAM_MASK] = data;
-        } 
-        
-    }
-
-    uint8_t read(uint16_t address) const {
-        if(address <= INTERNAL_RAM_MIRROR_END) {
-            return memory[address & INTERNAL_RAM_MASK];
-        }
-
-        return 0x00;
-    }
+    // Memory handling
+    void write(uint16_t address, uint8_t data);
+    uint8_t read(uint16_t address) const;
 
 
     // Cartridges
--- src/Bus.cpp
+++ src/Bus.cpp
@@ -2,6 +2,21 @@
 #include "Cartridge.h"
 
 
+// Functions for writing or reading into memory. Delegate memory or cartridge writing/reading through the given address
+void Bus::write(uint16_t address, uint8_t data) {
+    if(address <= INTERNAL_RAM_MIRROR_END) {
+        memory[address & INTERNAL_RAM_MASK] = data;
+    }
+}
+
+uint8_t Bus::read(uint16_t address) const {
+    if(address <= INTERNAL_RAM_MIRROR_END) {
+        return memory[address & INTERNAL_RAM_MASK];
+    }
+
+    return 0x00;
+}
+
 void Bus::insertCartridge(Cartridge* cart) {
     cartridge = cart;
 }
