2506046d52472ef52d14342159b5ca866c9ed8dd
nsameron2 <91213588+nsameron2@users.noreply.github.com>
2026-07-04 01:43:15 -0700

    feat: implement basic .nes rom running via main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
-#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
 
-int main() {
-    printf("hi\n");
-    return 0;
+#include "Bus.h"
+#include "CPU.h"
+#include "Cartridge.h"
+
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cerr << "USAGE: " << argv[0] << " [rom.nes]\n";
+        return EXIT_FAILURE;
+    }
+
+
+    Cartridge cart;
+    Bus bus;
+    CPU cpu;
+
+    if(!cart.load(argv[1])) {
+        std::cerr << "Failed to load ROM: " << argv[1] << '\n';
+        return EXIT_FAILURE;
+    }
+
+    bus.insertCartridge(&cart);
+    cpu.connectBus(&bus);
+    cpu.powerOn();
+
+    std::cout << "Loaded ROM: " << argv[1] << '\n';
+
+    // For now, runs until interrupted
+    while(true) {
+        cpu.clock();
+    }
+
+    return EXIT_SUCCESS;
 }
