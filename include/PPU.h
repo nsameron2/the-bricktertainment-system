f58e896605f1918d29b1b42c83f04ec2572dd131
nsameron2 <91213588+nsameron2@users.noreply.github.com>
2026-07-04 17:20:34 -0700

    feat: implement ppu vram reading and writing, complete with masking

--- include/PPU.h
+++ include/PPU.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <cstdint>
-#include <vector>
 #include <array>
 
 
 class PPU {
     public:
-        bool ppuWrite();
-        bool ppuRead() const;
 
     private:
         // PPU memory
         std::array<uint8_t, 2048> nametableRam{};
         std::array<uint8_t, 32> paletteRam{};
         std::array<uint8_t, 256> oam{};
-}
+
+        void writeVram(uint16_t address, uint8_t data);
+        uint8_t readVram(uint16_t address) const;
+};
